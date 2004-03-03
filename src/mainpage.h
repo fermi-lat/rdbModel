9f7f02a7332ce2628ed62322b255b773bfb45339
jrb <>
2004-03-03 18:38:39 +0000

    Add standard stuff for rdbModel

---
+++ src/mainpage.h
@@ -0,0 +1,29 @@
+// Mainpage for doxygen
+
+/** @mainpage package rdbModel
+
+  @section intro Introduction
+  This package contains classes and interfaces for describing and
+  accessing a relational database.  In particular it is designed with
+  the calibration metadata database in mind.  The only persistent input
+  form of the description supported at this time is XML; see 
+  @ref dtd  "rdbmsDescrip.dtd".
+
+  @section requirements requirements
+  @include requirements
+  <hr>
+  @section notes release.notes
+  release.notes
+  <hr> 
+
+  @todo    Write skeleton implementation for classes representing basic objects
+           described in xml: tables, columns, etc.
+  @todo    Figure out additional services these classes should provide
+           so that clients can communicate with database
+  @todo    Need method of entering database host, account information
+  @todo    gui
+  @todo    scripting
+           
+           
+ */
+
