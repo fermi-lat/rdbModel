a2c756a29d284bf3337343591b86b924b2492259
jrb <>
2004-04-08 01:34:17 +0000

    Store integer error code (if any) separately; add method to fetch it

--- rdbModel/RdbException.h
+++ rdbModel/RdbException.h
@@ -1,4 +1,4 @@
-// $Header:  $
+// $Header: /nfs/slac/g/glast/ground/cvs/rdbModel/rdbModel/RdbException.h,v 1.1 2004/03/27 01:37:25 jrb Exp $
 #ifndef RDBMODEL_RDBEXCEPTION_H
 #define RDBMODEL_RDBEXCEPTION_H
 #include <exception>
@@ -7,12 +7,14 @@ namespace rdbModel {
 
   class RdbException : std::exception {
   public:
-    RdbException(const std::string& extraInfo = "") : std::exception(),
-      m_name("RdbException"), m_extra(extraInfo) {}
+    RdbException(const std::string& extraInfo = "", int code=0) : 
+      std::exception(),
+      m_name("RdbException"), m_extra(extraInfo), m_code(code) {}
     virtual ~RdbException() throw() {}
     virtual std::string getMsg() {
       std::string msg = m_name + ": " + m_extra;
       return msg;}
+    virtual int getCode() const { return m_code;}
     virtual const char* what() {
       return m_extra.c_str();
     }
@@ -20,6 +22,7 @@ namespace rdbModel {
     std::string m_name;
   private:
     std::string m_extra;
+    int         m_code;
   };
 
 }
