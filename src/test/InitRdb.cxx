28db5aa587d798a70f049e28479cda341c9dd669
jrb <>
2006-10-12 23:41:55 +0000

    Fix debug output to write out primary key of new row, as intended.

--- src/test/InitRdb.cxx
+++ src/test/InitRdb.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/rdbModel/src/test/InitRdb.cxx,v 1.2 2006/10/11 23:53:55 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/rdbModel/src/test/InitRdb.cxx,v 1.3 2006/10/12 22:44:49 jrb Exp $
 // Class to initialize rdbModel-type database from init file
 // satisfying initRdbms.xsd schema, invoked from main initRdb.
 
@@ -178,7 +178,8 @@ namespace rdbModel {
     elts.clear();
     Dom::getChildrenByTagName(tblElt, std::string("row"), elts);
     for (unsigned iRow = 0; iRow < elts.size(); iRow++) {
-      ret = handleRow(elts[iRow], tbl, forAllFields);
+      unsigned newKey;
+      ret = handleRow(elts[iRow], tbl, forAllFields, &newKey);
       if (ret) return ret;
     }
     return 0;
@@ -219,8 +220,9 @@ namespace rdbModel {
     //    m_rdb->insertRow(tname, toInsert, signedKey, newKey);
     m_rdb->insertRow(tname, toInsert, 0, newKey);
     if (m_dbg) {
-      std::cout << "Inserted row into table " << tbl->getName() 
-                << ", assigned key " << newKey << std::endl;
+      std::cout << "Inserted row into table " << tbl->getName();
+      if (newKey) std::cout  << ", assigned key " << *newKey;
+      std::cout << std::endl;
     }
     return 0;
     // Should be ready to do insert 
