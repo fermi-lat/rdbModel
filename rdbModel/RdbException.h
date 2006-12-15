807428d703c3bdb724364ce17bf20f8a1d22b701
decot <>
2006-12-15 18:49:41 +0000

    what() exception method consistent with stdexcept

--- rdbModel/RdbException.h
+++ rdbModel/RdbException.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/rdbModel/rdbModel/RdbException.h,v 1.1 2004/03/27 01:37:25 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/rdbModel/rdbModel/RdbException.h,v 1.2 2004/04/08 01:34:16 jrb Exp $
 #ifndef RDBMODEL_RDBEXCEPTION_H
 #define RDBMODEL_RDBEXCEPTION_H
 #include <exception>
@@ -15,7 +15,7 @@ namespace rdbModel {
       std::string msg = m_name + ": " + m_extra;
       return msg;}
     virtual int getCode() const { return m_code;}
-    virtual const char* what() {
+    virtual const char* what() const throw() {
       return m_extra.c_str();
     }
   protected: 
