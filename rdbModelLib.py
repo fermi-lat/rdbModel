6a20e145a5b76e65eed86ed9232cc045e601a2f3
jrb <>
2012-08-17 18:49:48 +0000

    patches for Windows no-install-headers

--- rdbModelLib.py
+++ rdbModelLib.py
@@ -1,8 +1,10 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/rdbModel/rdbModelLib.py,v 1.1 2008/03/28 17:56:36 jrb Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/rdbModel/rdbModelLib.py,v 1.2 2008/06/30 20:18:34 glastrm Exp $
 # rdbModelLib.py
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['rdbModel'], package = 'rdbModel')
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'rdbModel') 
     env.Tool('facilitiesLib')
     env.Tool('xmlBaseLib')
     env.Tool('addLibrary', library = env['xercesLibs'])
